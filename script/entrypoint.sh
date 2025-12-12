8819e7fc65b391f315775d06619968772e91c795
moe <133772407+kalknord@users.noreply.github.com>
2025-12-12 20:08:49 +0100

    fix kafka

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,19 +1,20 @@
-#!bin/bash
+#!/bin/bash
 set -e
 
-if[ -e '/opt/airflow/requirements.txt']; then
-    $(command -v pip) install --user -r requirements.txt
+if [ -e "/opt/airflow/requirements.txt" ]; then
+  $(command python) pip install --upgrade pip
+  $(command -v pip) install --user -r requirements.txt
 fi
 
-if [ ! -f 'opt/airflow/airflow.db' ]; then
-    airflow db init && \
-    airflow users create \
-        --username admin \
-        --firstname Admin \
-        --lastname User \
-        --role Admin \
-        --email admin@example.com \
-        --password admin
+if [ ! -f "/opt/airflow/airflow.db" ]; then
+  airflow db init && \
+  airflow users create \
+    --username admin \
+    --firstname admin \
+    --lastname admin \
+    --role Admin \
+    --email admin@example.com \
+    --password admin
 fi
 
 $(command -v airflow) db upgrade
