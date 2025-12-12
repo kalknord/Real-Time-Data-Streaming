9ae831c6841d69340265fe77e48618cf28a7ee1c
moe <133772407+kalknord@users.noreply.github.com>
2025-12-12 19:34:39 +0100

    test webserver

--- dags/kafka-stream.py
+++ dags/kafka-stream.py
@@ -36,10 +36,15 @@ def format_data(res):
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
+    
     res = get_data()   
     res = format_data(res)
-    print(json.dumps(res, indent=4))
-    
+    # print(json.dumps(res, indent=4))
+
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
 
 
 
